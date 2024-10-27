c9adf7acf26b374fb9df35244f4e23cc0c523c51
Baggio25 <rodrigo.baggio.si@gmail.com>
2024-10-26 23:02:09 -0300

    Exibindo a cotação da moeda

--- scripts.js
+++ scripts.js
@@ -9,6 +9,7 @@ const form = document.querySelector("form");
 const amount = document.getElementById("amount");
 const currency = document.getElementById("currency");
 const footer = document.querySelector("main footer");
+const description = document.getElementById("description");
 
 
 //Manipulando o input amount para receber somente números
@@ -39,8 +40,11 @@ form.onsubmit = (event) => {
 function convertCurrency(amount, price, symbol) {
   try {
 
+    description.textContent = `${symbol} 1 = ${price}`
+
     //Aplica a classe que exibe o footer
     footer.classList.add("show-result");
+
   } catch (error) {
     console.log(error);
 
