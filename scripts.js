b0466820cc84b4e8c521da6e54bec0f25c2ba348
Baggio25 <rodrigo.baggio.si@gmail.com>
2024-10-26 22:57:18 -0300

    Exibindo o footer

--- scripts.js
+++ scripts.js
@@ -8,6 +8,7 @@ const GBP = 7.40;
 const form = document.querySelector("form");
 const amount = document.getElementById("amount");
 const currency = document.getElementById("currency");
+const footer = document.querySelector("main footer");
 
 
 //Manipulando o input amount para receber somente números
@@ -36,7 +37,17 @@ form.onsubmit = (event) => {
 
 //Função para converter a moeda
 function convertCurrency(amount, price, symbol) {
-  console.log(amount, price, symbol);
+  try {
+
+    //Aplica a classe que exibe o footer
+    footer.classList.add("show-result");
+  } catch (error) {
+    console.log(error);
+
+    //Remove a classe do footer ocultando ele.
+    footer.classList.remove("show-result");
+    alert("Não foi possível converter, tente novamente mais tarde.");
+  }
 }
 
 
